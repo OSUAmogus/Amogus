9ebc0bf61ed2b049cb283cc403f68ca45eb27199
Kieran Hostetler-McLaughlin <kmhm24@gmail.com>
2021-11-10 20:03:18 -0800

    hey, here's some cool player movement. it's very cool, right?

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //declare variables
+    public float moveSpeed; //movement speed. can be adjusted in editor
+    public Rigidbody2D rb;  //assign player rigidbody to script
+    Vector2 movement;  //Vector2 variable that's important for player responding to keystokes/joystick
+
+    // Update is called once per frame
+    void Update() 
+    {
+        //updates to movement.x and movement.y allow player to respond to WASD/joystick
+        movement.x = Input.GetAxisRaw("Horizontal");  
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Fixed update is called on a set timer and is better for physics
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
