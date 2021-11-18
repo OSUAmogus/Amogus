fbe39ff85a5ae946badd7803f94633f5add4ab07
Adrian Baker <bakerad@oregonstate.edu>
2021-11-17 20:38:39 -0800

    refactored movement system

--- Assets/AmogusPlayer.cs
+++ Assets/AmogusPlayer.cs
@@ -25,22 +25,28 @@ public class AmogusPlayer : MonoBehaviour
         if (photonView.IsMine)
         {
 
-            if (Input.GetKey("w"))
-            {
-                transform.position += new Vector3(0.0f, 0.01f, 0.0f);
-            }
-            if (Input.GetKey("a"))
-            {
-                transform.position += new Vector3(-0.01f, 0.0f, 0.0f);
-            }
-            if (Input.GetKey("s"))
-            {
-                transform.position += new Vector3(0.0f, -0.01f, 0.0f);
-            }
-            if (Input.GetKey("d"))
-            {
-                transform.position += new Vector3(0.01f, 0.0f, 0.0f);
-            }
+            //if (Input.GetKey("w"))
+            //{
+            //    transform.position += new Vector3(0.0f, 0.01f, 0.0f);
+            //}
+            //if (Input.GetKey("a"))
+            //{
+            //    transform.position += new Vector3(-0.01f, 0.0f, 0.0f);
+            //}
+            //if (Input.GetKey("s"))
+            //{
+            //    transform.position += new Vector3(0.0f, -0.01f, 0.0f);
+            //}
+            //if (Input.GetKey("d"))
+            //{
+            //    transform.position += new Vector3(0.01f, 0.0f, 0.0f);
+            //}
+            //Debug.Log(Input.GetAxis("Horizontal"));
+            transform.position += new Vector3(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"),
+                0.0f
+            ) * 0.01f;
 
         }
     }
